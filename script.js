76bd6bb7ae108c10a0973bc65c3325818a701763
Renuka <95637547+AdvikBankar@users.noreply.github.com>
2025-10-30 22:46:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // Navbar js
-  var toggleOpen = document.getElementById('toggleOpen');
-  var toggleClose = document.getElementById('toggleClose');
-  var collapseMenu = document.getElementById('collapseMenu');
+var toggleOpen = document.getElementById('toggleOpen');
+var toggleClose = document.getElementById('toggleClose');
+var collapseMenu = document.getElementById('collapseMenu');
 
-  function handleClick() {
-    if (collapseMenu.style.display === 'block') {
-      collapseMenu.style.display = 'none';
-    } else {
-      collapseMenu.style.display = 'block';
-    }
+function handleClick() {
+  if (collapseMenu.style.display === 'block') {
+    collapseMenu.style.display = 'none';
+  } else {
+    collapseMenu.style.display = 'block';
   }
+}
 
-  toggleOpen.addEventListener('click', handleClick);
-  toggleClose.addEventListener('click', handleClick);
-
-});
+toggleOpen.addEventListener('click', handleClick);
+toggleClose.addEventListener('click', handleClick);
